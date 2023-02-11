ad0f823d3d1731c32f447b925bbdc6f4d8d412c3
Christian Münch <christian@muench-worms.de>
2023-02-11 09:54:13 +0100

    Add path export

--- zsh-magento-cloud.plugin.zsh
+++ zsh-magento-cloud.plugin.zsh
@@ -1,9 +1,11 @@
 #!/usr/bin/env zsh
 # shellcheck disable=SC1090
 
+export PATH=$PATH:$HOME/.magento-cloud/bin
+
 # Exit if the 'magento-cloud' command can not be found
 if ! (( $+commands[magento-cloud] )); then
-    echo "ERROR: 'gcloud' command not found"
+    echo "ERROR: 'magnento-cloud' command not found"
     return
 fi
 
